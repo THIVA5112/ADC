1342a2ba353780c66a2235c9e48fffbf933df3de
Thivagar Subramaniyan <thivagarsubramanian@gmail.com>
2025-08-17 22:46:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -408,7 +408,7 @@ app.get('/patients/:id', async (req, res) => {
   res.json(patient);
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
