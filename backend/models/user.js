453ee36baa6fb0d32c2ba4d0bb0517e8a9d1609f
Thivagar Subramaniyan <thivagarsubramanian@gmail.com>
2025-09-03 02:11:04 +0530

    Add branch field to user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }, // Store hashed password!
   capabilityLevel: { type: Number, default: 1 }, // 1: User, 2: Admin, 3: Manager
+  branch: { type: String, required: true }, // e.g., 'Lalgudi', 'Trichy'
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
