225c4120fc2970a4e78389a716db1f20bb176447
Thivagar Subramaniyan <thivagarsubramanian@gmail.com>
2025-09-03 18:58:20 +0530

    Reorder import statements in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
+import express from 'express';
+import mongoose from 'mongoose';
 import cors from 'cors';
 import bcrypt from 'bcryptjs';
 import moment from 'moment';
